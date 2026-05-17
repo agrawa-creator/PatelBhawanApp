1cbbb521125eeba3636b60f049bfb403e012d5b3
agrawa-creator <agarwalchirag4391@gmail.com>
2026-05-17 17:45:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ KEY = "sb_publishable_RQuXJ1BP3wpLnWmp3WLMvQ_vT5mxYq4"
 supabase = create_client(URL, KEY)
 
 # --- 2. TELEGRAM CONFIG ---
-TELE_TOKEN = "8880275866: AAHaZ6Zd1S-X5M0oxmAni6mxHC8vgTIntPA"
+TELE_TOKEN = "8880275866:AAHaZ6Zd1S-X5M0oxmAni6mxHC8vgTIntPA"
 CHAT_IDS = ["7261699388", "7609324930"]
 
 def send_tele_msg(text):
@@ -125,13 +125,13 @@ with col_cart:
         st.write(f"### Total Bill: ₹{total_bill}")
         
         # Address Details
-        hostel_list = ["Patel Bhavan",]
+        hostel_list = ["Pandit Lekhram",]
         h_choice = st.selectbox("Select Your Hostel", hostel_list)
         
         # PICKUP LOGIC
         is_pickup = False
         final_hostel = h_choice
-        if h_choice != "Patel Bhavan":
+        if h_choice != "Pandit Lekhram":
             is_pickup = True
             if h_choice == "Other":
                 final_hostel = st.text_input("Hostel Ka Naam Likhein")
